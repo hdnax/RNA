d771e49dddcf370a3055ae0c0f4a023619eab31b
Huy-DNA <huydo862003@gmail.com>
2026-02-03 09:22:52 +0700

    chore: init stdx

---
+++ packages/stdx/README.md
@@ -0,0 +1,7 @@
+# Stdx
+
+![Status](https://img.shields.io/badge/status-active-brightblue)
+![Version](https://img.shields.io/badge/version-0.1.0--alpha-orange)
+![License](https://img.shields.io/badge/license-MIT-green)
+
+Shared useful utilities.
---
+++ packages/stdx/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({ tsconfigPath: './tsconfig.json' }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Stdx',
+      fileName: 'stdx',
+    },
+  },
+});
